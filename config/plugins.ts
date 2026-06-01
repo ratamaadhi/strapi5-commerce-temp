c4c9b92f608baa2a86674bcc624d9f67c731b685
Ratama Adhi <ratama.adhi@gmail.com>
2026-06-01 20:39:36 +0700

    feat: configure aws-s3 upload provider for MinIO

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,30 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('MINIO_ACCESS_KEY'),
+            secretAccessKey: env('MINIO_SECRET_KEY'),
+          },
+          region: env('MINIO_REGION', 'us-east-1'),
+          endpoint: env('MINIO_ENDPOINT'),
+          forcePathStyle: true,
+          params: {
+            Bucket: env('MINIO_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
 
 export default config;
