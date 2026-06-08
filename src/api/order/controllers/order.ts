e6c1116aa6e5051a7ba3626fadab0272369dc823
Ratama Adhi <ratama.adhi@gmail.com>
2026-06-08 20:55:18 +0700

    feat: override order find to filter by current user

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -4,4 +4,27 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::order.order');
+export default factories.createCoreController('api::order.order', ({ strapi }) => ({
+  async find(ctx) {
+    await this.validateQuery(ctx);
+
+    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+
+    if (ctx.state.user) {
+      sanitizedQueryParams.filters = {
+        ...sanitizedQueryParams.filters,
+        user: {
+          documentId: { $eq: ctx.state.user.documentId },
+        },
+      };
+    }
+
+    const { results, pagination } = await strapi
+      .service('api::order.order')
+      .find(sanitizedQueryParams);
+
+    const sanitizedResults = await this.sanitizeOutput(results, ctx);
+
+    return this.transformResponse(sanitizedResults, { pagination });
+  },
+}));
