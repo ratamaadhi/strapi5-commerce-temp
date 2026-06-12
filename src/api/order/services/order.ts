caf3d8b316f01dfc15ea47dc8b5fd7090957f762
Ratama Adhi <ratama.adhi@gmail.com>
2026-06-13 00:19:12 +0700

    feat: add cancelOrder service

--- src/api/order/services/order.ts
+++ src/api/order/services/order.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::order.order');
+export default factories.createCoreService('api::order.order', ({ strapi }) => ({
+  async cancelOrder(documentId: string) {
+    const result = (await strapi.documents('api::order.order').update({
+      documentId,
+      data: { orderStatus: 'cancelled' },
+    })) as any;
+
+    strapi.log.info(`Order ${result.orderNumber} cancelled`);
+    return result;
+  },
+}));
